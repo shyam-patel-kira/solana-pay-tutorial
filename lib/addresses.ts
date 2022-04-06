0981960268819fc763ebc7f6747a1833060c83b8
Shyam Patel <shyampkira@gmail.com>
2022-04-07 00:07:47 +0530

    Completed solana pay tutorial

--- lib/addresses.ts
+++ lib/addresses.ts
@@ -1,4 +1,8 @@
 import { PublicKey } from "@solana/web3.js"
 
 // Your shop wallet address
-export const shopAddress = new PublicKey('2t18we2LNm69VPjhhyfMPzBcwzmzDh1kg8NgApjk9YYU') 
+export const shopAddress = new PublicKey('2t18we2LNm69VPjhhyfMPzBcwzmzDh1kg8NgApjk9YYU')
+
+export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
+
+export const couponAddress = new PublicKey('74aFyMgbKppdab4wEVz5AXL3UGbxMkwkwgK2LVzn3MmG')
